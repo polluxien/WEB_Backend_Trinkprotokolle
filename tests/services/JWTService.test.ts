526876209f03fc466504b918234bd2312d9b9dae
Bennet <s86545@bht-berlin.de>
2024-06-07 12:54:32 +0200

    verifyJWT implemnetiert, könnte fehler enthalten, mit cookies nicht gearbeitet und tests fehelen

---
+++ tests/services/JWTService.test.ts
@@ -0,0 +1,27 @@
+import { verifyPasswordAndCreateJWT } from "../../src/services/JWTService";
+import { createPfleger } from "../../src/services/PflegerService";
+
+
+beforeEach(async () => {
+  await createPfleger({
+    name: "Hofrat Behrens",
+    password: "12345bcdABCD..;,.",
+    admin: false,
+  });
+});
+
+test("verifyPasswordAndCreateJWT sollte jwt empfangen", async () => {
+  const maJwt = await verifyPasswordAndCreateJWT(
+    "Hofrat Behrens",
+    "12345bcdABCD..;,."
+  );
+  expect(maJwt).toBeDefined();
+});
+
+test("verifyPasswordAndCreateJWT sollte nicht jwt empfangen", async () => {
+  const maJwt = await verifyPasswordAndCreateJWT(
+    "Horat Behren",
+    "12345bcdABCD..;,."
+  );
+  expect(maJwt).not.toBeDefined();
+});
